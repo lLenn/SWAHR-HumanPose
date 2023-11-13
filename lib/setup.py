636d3ed084fdbede3b0098bf3a5afd8618e650a5
Tristan <tverheecke@hotmail.com>
2023-11-13 17:50:58 +0100

    setuptools

--- lib/setup.py
+++ lib/setup.py
@@ -1,11 +1,7 @@
 from setuptools import setup, find_packages
 
-packages = find_packages(where="lib")
-print(packages)
+packages = find_packages(where="SWAHR")
 setup(
     name="SWAHR",
     packages=["SWAHR"] + [f"SWAHR.{package}" for package in packages],
-    package_dir={
-        "": "lib"
-    }
 )
